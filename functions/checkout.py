74d92e7bbd690bd90018e275329f4394b77b9383
Nash Subedi <dobbysubedi@gmail.com>
2023-01-25 09:52:23 +0000

    added parameter for commit hash in checkout and set current commit to full commit hash instead of the hash ID of 10 characters

--- functions/checkout.py
+++ functions/checkout.py
@@ -11,8 +11,8 @@ def _get_commit_hash():
             return commit_hash
         print("Invalid commit hash.")
 
-def checkout():
-    commit_hash_id = _get_commit_hash()
+def checkout(commit_hash_id=""):
+    commit_hash_id = _get_commit_hash() if commit_hash_id == "" else commit_hash_id
     print("Checking out commit: " + commit_hash_id)
 
     commit_file = os.path.join(get_working_directory(), '.suv', 'commits', 'commit-metadata.suv')
@@ -22,7 +22,7 @@ def checkout():
         commit_path = get_commit_path(commit_hash_id, commit_tree)
         remove_directory_content(get_working_directory())
         update_directory_content(get_working_directory(), commit_path)
-        set_current_commit(commit_hash_id)
+        set_current_commit(commit_tree.get_node(commit_hash_id).tag)
 
     else:
         print("Commit not found.")
