7e21ccd902689c5f22cd11454f8106222ab9bfa8
Nash Subedi <dobbysubedi@gmail.com>
2023-01-25 09:53:10 +0000

    added code to append commit hash for a branch on each commit

--- functions/commit.py
+++ functions/commit.py
@@ -1,7 +1,9 @@
 from functions.diff import diff
 from functions.users import get_author
+
 from helpers.app_helpers import check_init, get_working_directory
 from helpers.commit_helpers import update_head_at_commit, store_commit, add_commit_to_tree, set_current_commit
+from helpers.branch_helpers import get_branch_data, store_branch_data
 
 import json
 
@@ -39,7 +41,11 @@ def commit():
 
         commit_data['hash'] = commit_hash
         commit_data['timestamp'] = int(datetime.utcnow().timestamp())
+
+        branch_data = get_branch_data()
+        branch_data[branch_data['current_branch']]['commits'].append(commit_hash)
         
+        store_branch_data(branch_data)
         store_commit(commit_data)
         add_commit_to_tree(commit_hash)
         set_current_commit(commit_hash)
